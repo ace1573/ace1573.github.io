69f48fd42a362e486ea4527b7fdf40b0e8c25add
CrazyPig <18825111236@163.com>
2016-03-04 17:33:53 +0800

    feat : 增加与我联系实现
    增加模态框来加载与我联系的方式，包括显示邮箱地址,
    微信和qq二维码

--- js/default.js
+++ js/default.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
 	// ....
 });
+
+var showEmail = function() {
+	// alert('showEmail is called');
+	var myModalBody = $('#myModalBody');
+	var content = '<p>我的邮箱 : 18825111236@163.com</p>';
+	myModalBody.html(content);
+	$('#myModal').modal({
+		show: true
+	});
+};
+
+var showWeixin2DCode = function() {
+	// alert('showWeixin2DCode is called');
+	var myModalBody = $('#myModalBody');
+	var content = '<img src="/img/weixin_erweima.jpg" width="50%" height="50%" />';
+	myModalBody.html(content);
+	$('#myModal').modal({
+		show: true
+	});
+};
+
+var showQQ2DCode = function() {
+	// alert('showQQ2DCode is called');
+	var myModalBody = $('#myModalBody');
+	var content = '<img src="/img/qq_erweima.jpg" width="50%" height="50%" />';
+	myModalBody.html(content);
+	$('#myModal').modal({
+		show: true
+	});
+};
