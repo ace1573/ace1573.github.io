2487cd032a7113645740979abc9c2a874f1bdf02
CrazyPig <18825111236@163.com>
2016-03-04 14:38:16 +0800

    feat : 介绍面板增加新的展示内容
    1.介绍面板增加新的展示内容,包括"标签云"、"最新发表"等子面板
    2.调整界面布局,以更好适应移动端展示

--- js/default.js
+++ js/default.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-	var mainPanelHeight = $('#main-panel').height();
-	var introPanelHeight = $('#intro-panel').height();
-	console.log("mainPanelHeight : " + mainPanelHeight + ", introPanelHeight : " + introPanelHeight);
-	if(mainPanelHeight < introPanelHeight) {
-		// adjust main-panel height to intro-panel height
-		$('#main-panel').css("height", introPanelHeight);
-	} else if(mainPanelHeight > introPanelHeight) {
-		// adjust intro-panel height to main-panel height
-		$('#intro-panel').css("height", mainPanelHeight);
-	}
+	// ....
 });
